2be3a283c3b426ecf8bc72f534666918227c6bbc
Clinton Freeman <freeman@impulsonic.com>
2016-03-05 10:27:05 -0500

    Moved ScenePointSet_3 implementation to cpp.

--- rationalcad/scene_pointset.cpp
+++ rationalcad/scene_pointset.cpp
@@ -12,3 +12,65 @@
  * more details. You should have received a copy of the GNU General Public
  * License along with RationalCAD. If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include "scene_pointset.h"
+
+namespace RCAD {
+
+ScenePointSet_3::ScenePointSet_3() {
+    model_point_set_.AddObserver(this);
+}
+
+void ScenePointSet_3::Initialize(const QVector2D& point) {
+    model_point_set_.add(Point_3r(point.x(), point.y(), 0));
+}
+
+void ScenePointSet_3::Initialize(const QVector<QVector3D>& data) {
+    for (auto point : data) {
+        model_point_set_.add(Point_3r(point.x(), point.y(), point.z()));
+    }
+}
+
+void ScenePointSet_3::Update(const QVector2D& point) {
+    model_point_set_.add(Point_3r(point.x(), point.y(), 0));
+}
+
+void ScenePointSet_3::Select() {
+    Visual::Material selected_mat;
+    selected_mat.set_ambient(Visual::Color::SKYBLUE);
+
+    for (auto point : model_point_set_.points()) {
+        SigPushVisualPoint_3r(*point, Visual::Point(selected_mat));
+    }
+}
+
+void ScenePointSet_3::Deselect() {
+    for (auto point : model_point_set_.points()) {
+        SigPopVisualPoint_3r(*point);
+    }
+}
+
+Intersection::Ray_3rSceneObject ScenePointSet_3::intersect(const Ray_3r &ray) {
+    Intersection::Toleranced::Ray_3rPointSet_3r isect(&ray, &model_point_set_);
+    return Intersection::Ray_3rSceneObject(isect.type() == Intersection::Toleranced::Ray_3rPointSet_3r::INTERSECTION_EMPTY, isect.time());
+}
+
+void ScenePointSet_3::UpdateColor(const QColor &color) {}
+
+QString ScenePointSet_3::scene_object_type() const {
+    return "PointSet";
+}
+
+const QString& ScenePointSet_3::name() const {
+    return name_;
+}
+
+void ScenePointSet_3::set_name(const QString &name) {
+    name_ = name;
+}
+
+const PointSet_3r& ScenePointSet_3::model_point_set() const {
+    return model_point_set_;
+}
+
+}
--- rationalcad/scene_pointset.h
+++ rationalcad/scene_pointset.h
@@ -26,60 +26,29 @@ namespace RCAD {
 
 class ScenePointSet_3 : public ISceneObject, public Visual::Geometry {
 public:
-    ScenePointSet_3() {
-        model_point_set_.AddObserver(this);
-    }
+    ScenePointSet_3();
 
-    void Initialize(const QVector2D& point) {
-        model_point_set_.add(Point_3r(point.x(), point.y(), 0));
-    }
+    void Initialize(const QVector2D& point);
 
-    void Update(const QVector2D& point) {
-        model_point_set_.add(Point_3r(point.x(), point.y(), 0));
-    }
+    void Initialize(const QVector<QVector3D>& data);
 
-    void Initialize(const QVector<QVector3D>& data) {
-        for (auto point : data) {
-            model_point_set_.add(Point_3r(point.x(), point.y(), point.z()));
-        }
-    }
+    void Update(const QVector2D& point);
 
-    void Select() override {
-        Visual::Material selected_mat;
-        selected_mat.set_ambient(Visual::Color::SKYBLUE);
+    void Select() override;
 
-        for (auto point : model_point_set_.points()) {
-            SigPushVisualPoint_3r(*point, Visual::Point(selected_mat));
-        }
-    }
+    void Deselect() override;
 
-    void Deselect() override {
-        for (auto point : model_point_set_.points()) {
-            SigPopVisualPoint_3r(*point);
-        }
-    }
+    Intersection::Ray_3rSceneObject intersect(const Ray_3r &ray) override;
 
-    Intersection::Ray_3rSceneObject intersect(const Ray_3r &ray) {
-        Intersection::Toleranced::Ray_3rPointSet_3r isect(&ray, &model_point_set_);
-        return Intersection::Ray_3rSceneObject(isect.type() == Intersection::Toleranced::Ray_3rPointSet_3r::INTERSECTION_EMPTY, isect.time());
-    }
+    void UpdateColor(const QColor &color) override;
 
-    void UpdateColor(const QColor &color) override {}
+    QString scene_object_type() const override;
 
-    QString scene_object_type() const override {
-        return "PointSet";
-    }
+    const QString& name() const override;
 
-    const QString& name() const override {
-        return name_;
-    }
-    void set_name(const QString &name) override {
-        name_ = name;
-    }
+    void set_name(const QString &name) override;
 
-    const PointSet_3r& model_point_set() const {
-        return model_point_set_;
-    }
+    const PointSet_3r& model_point_set() const;
 
 private:
     PointSet_3r model_point_set_;
